fb8daa0a57a26df8d0ab021adf57701acfdd22ac
Michael Timbrook <timbrook480@gmail.com>
2018-12-13 21:11:19 -0800

    Don't need that file

--- filter.py
+++
@@ -1,7 +0,0 @@
-from contextlib import suppress
-
-with open("/Users/timbrook/Downloads/data-download-pub78.txt") as data, open("/Users/timbrook/Downloads/foundations_import.csv", "w") as output:
-  for line in data.readlines():
-    with suppress(IndexError):
-      ent = line.split('|')[1]
-      output.write(f"\"{ent}\", \"{ent}\"\n")
